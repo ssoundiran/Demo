b26f227f1b93a7c3bdf910ad2f883436f3069e60
ssoundiran <49755794+ssoundiran@users.noreply.github.com>
2020-05-06 17:30:28 +0530

    RMA bundle

---
+++ CEP_CreateRMAContractPageHelper.js
@@ -0,0 +1,9 @@
+({
+	onChangeCountryHelper: function(component, event, helper) {
+        var country = component.get("v.shipCountry");
+        console.log("country: " + country);
+        if (country == 'United States') {
+            component.set("v.stateListShow", true);
+        }
+    },
+})
